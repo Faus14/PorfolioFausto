ccdcf12f5ade5008ec13a273c4d5a8d1640e3fe5
Faus14 <fausaludas14@gmail.com>
2025-03-07 01:40:24 -0300

    version 1.0

--- app/page.js
+++ app/page.js
@@ -1,24 +1,9 @@
 // app/page.jsx
-"use client"; // Añadir esta directiva al inicio
+import ClientPage from "./client-page";
 
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Skills from "./components/homepage/skills";
-import Projects from "./components/homepage/projects";
+// Deshabilitar generación estática
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return (
-    <div suppressHydrationWarning>
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Education />
-      <Projects />
-      <ContactSection />
-    </div>
-  );
+  return <ClientPage />;
 }
