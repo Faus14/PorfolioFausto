82b57fb2b08d1ffd37da7893be46efc7c3760820
Faus14 <fausaludas14@gmail.com>
2025-03-07 21:37:32 -0300

    version 1.8

--- app/client-page.jsx
+++ app/client-page.jsx
@@ -1,10 +1,15 @@
 "use client";
 import HeroSection from "./components/homepage/hero-section";
+import AboutSection from "./components/homepage/about";
+import Experience from "./components/homepage/experience";
+
 
 export default function ClientPage() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
+      <Experience />
     </div>
   );
 }
