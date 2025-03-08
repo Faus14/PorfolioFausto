200209d2f9ba2083a521fa3e1e4fd85582531798
Faus14 <fausaludas14@gmail.com>
2025-03-07 21:30:08 -0300

    version 1.8

--- app/client-page.jsx
+++ app/client-page.jsx
@@ -1,23 +1,5 @@
 "use client";
 
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Skills from "./components/homepage/skills";
-import Projects from "./components/homepage/projects";
-
 export default function ClientPage() {
-  return (
-    <div suppressHydrationWarning>
-      <HeroSection />
-      <AboutSection />
-      <Experience />
-      <Skills />
-      <Education />
-      <Projects />
-      <ContactSection />
-    </div>
-  );
+  return <div>Prueba simple</div>;
 }
