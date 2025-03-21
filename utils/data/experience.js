84a8ec182b14f424ba08c39571cbae943fdc14aa
Faus14 <fausaludas14@gmail.com>
2025-03-21 00:26:18 -0300

    modifico info

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,15 @@
 export const experiences = [
   {
-    id: 1,
+    id: 2,
     title: 'Sysadmin',
     company: "Chatealo",
     duration: "(Dec 2023 - Jun 2024)",
     description: "Implemented Docker containers to enhance system management and scalability. Created comprehensive project documentation, efficiently resolved technical issues, and collaborated effectively with cross-functional teams. Managed system and application configurations to maintain optimal performance and reliability."
   },
   {
-    id: 2,
+    id: 1,
     title: 'Full Stack Developer',
-    duration: "(Dec 2023 - Jun 2024)",
+    duration: "(Jan 2023 - Mar 2024)",
     description: "Developed and maintained web applications using JavaScript, TypeScript, and Node.js. Designed RESTful APIs and integrated third-party services to enhance functionality. Implemented database solutions with PostgreSQL, ensuring efficient data storage and retrieval. Optimized application performance and collaborated closely with UI/UX designers to improve user experience."
   },
   {
