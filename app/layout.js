12b50d8141444bcf7501632c4e0055731a44af5b
Fausto Saludas <fausaludas14@gmail.com>
2025-10-18 18:52:38 -0300

    nuevas cambios para hacer mas rapido el portafolio

--- app/layout.js
+++ app/layout.js
@@ -27,8 +27,8 @@ export default function RootLayout({ children }) {
       <head>
         {/* ✅ Preload critical resources */}
         <link rel="preload" href="/profile.jpeg" as="image" type="image/jpeg" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body 
         className={inter.className}
